b92a47da556e5b24796fb614971bf08a3c5d1d1e
Cesar Maquera <cmaquerat@gmail.com>
2023-10-10 01:35:31 -0500

    feat: add new weights

--- lpdr/detection/detection.py
+++ lpdr/detection/detection.py
@@ -1,14 +1,14 @@
 import torch
 import cv2
 
-model = torch.hub.load('lpdr/detection/yolov5', 'custom', path='lpdr/detection/weigths/best_yolo_cbam.pt', source='local')
+model = torch.hub.load('lpdr/detection/yolov5', 'custom', path='lpdr/detection/weigths/best_yolo_cbam_v2.pt', source='local')
 
 def get_plates_image(image):
 
     df_results = model(image).pandas().xyxy[0]
     df_detections = df_results[df_results["confidence"] > 0.1]
 
-    if len(df_detections) is not 0 :
+    if len(df_detections) != 0 :
         plate_crop = cv2.imencode('.jpg', image[int(df_detections["ymin"]):int(df_detections["ymax"]), int(df_detections["xmin"]):int(df_detections["xmax"]), :])[1].tobytes()
 
         df_detections["image"] = plate_crop 
